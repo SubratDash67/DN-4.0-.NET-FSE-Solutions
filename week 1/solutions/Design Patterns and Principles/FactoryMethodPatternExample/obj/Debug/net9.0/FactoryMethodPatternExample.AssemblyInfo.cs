e6afd295c995a8d073a496521eb2d007dc281610
SubratDash67 <subratdash2022@gmail.com>
2025-06-16 17:26:19 +0530

    updated output of codes

--- week 1/solutions/Design Patterns and Principles/FactoryMethodPatternExample/obj/Debug/net9.0/FactoryMethodPatternExample.AssemblyInfo.cs
+++ week 1/solutions/Design Patterns and Principles/FactoryMethodPatternExample/obj/Debug/net9.0/FactoryMethodPatternExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactoryMethodPatternExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a5764bf161372bc64212715ab75c0a70b5ed10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55733e79917a05564998dc0ae21282994b209152")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactoryMethodPatternExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactoryMethodPatternExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
